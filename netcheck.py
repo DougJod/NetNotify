cc4b58e9067349a5f8e93933c746bbcb452900ec
Doug Jodrell <developer@untetheredcomputing.com>
2017-11-14 16:05:29 +0000

    initial checkin

---
+++ netcheck.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python3
+import netifaces as ni
+import json
+import urllib.parse
+import urllib.request
+import urllib.error
+import datetime
+import os
+#from os.path import expanduser
+
+HOME = os.environ['HOME'] 
+#HOME = expanduser('~')
+SFDC_LOGIN_URL = 'https://login.salesforce.com/services/oauth2/token'
+SFDC_KEY = os.environ['SFDC_KEY'] 
+SFDC_SECRET = os.environ['SFDC_SECRET'] 
+SFDC_MENTION_ID = os.environ['SFDC_MENTION_ID']
+SFDC_USER = os.environ['SFDC_USER'] 
+SFDC_PASS = os.environ['SFDC_PASS'] 
+SFDC_TOKEN = os.environ['SFDC_TOKEN'] 
+
+############################################################################
+# Authenticate to SalesForce.
+# Return oauth data if successful.
+####################################
+def sfdcAuthenticate() :
+	oauthData = {}
+	reqData={}
+	reqData['grant_type'] = 'password'
+	reqData['client_id'] = SFDC_KEY
+	reqData['client_secret'] = SFDC_SECRET
+	reqData['username'] = SFDC_USER
+	reqData['password'] = SFDC_PASS+SFDC_TOKEN
+	reqData = urllib.parse.urlencode(reqData) 
+	req = urllib.request.Request(SFDC_LOGIN_URL, data=reqData.encode('utf-8'))
+	req.add_header('content-type','application/x-www-form-urlencoded')
+
+	try :
+		response = urllib.request.urlopen(req)
+		oauthData = json.loads(response.read().decode('utf-8'))
+		#print ('got oauthData: ', oauthData)
+	except urllib.error.URLError as e:
+		print ('could not send request:', e.reason)
+
+	return oauthData
+
+##############################################################################
+# Append the old IP address to the ip change log file. 
+#######################################################
+def logOldIp(oldIp) :
+	log = open(HOME+'/ip_log.txt', 'a')
+	log.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' - Old IP: '+oldIp +'\n')
+	log.close()
+
+##############################################################################
+# Check if the WiFi IP address has changed.
+###########################################
+def hasIpChanged(currentIp) :
+	ipChanged = False
+	# read last IP from file	
+	f = open(HOME+'/last_ip.txt', 'r+')
+	lastIp = f.read()
+	if (lastIp != currentIp) :
+		ipChanged = True
+		logOldIp(lastIp)
+		f.seek(0)
+		f.write(currentIp)
+		f.truncate()
+	f.close()
+	return ipChanged
+
+##############################################################################
+# Send Chatter Notification to SFDC.
+####################################
+def sendChangeNotification(currentIp) :
+
+	oauthData = sfdcAuthenticate()
+	if 'access_token' in oauthData :
+	
+		chatterUrl = oauthData['instance_url']+'/services/data/v39.0/chatter/feed-elements';
+
+		# message header
+		headerSegment = {}
+		headerSegment['type']='Text'
+		headerSegment['text']='[#AutomatedMessage]\n'
+		# @mention
+		messageSegment = {}
+		messageSegment['type']='Text'
+		messageSegment['text']=' - Your Raspberry Pi IP has changed to: '+ currentIp
+		# post body
+		mentionSegment = {}
+		mentionSegment['type']='mention'
+		mentionSegment['id']=SFDC_MENTION_ID
+
+		chatterPost = {}
+		chatterPost['feedElementType'] = 'FeedItem'
+		chatterPost['subjectId'] = 'me'
+		chatterPost['body'] = {} 
+		chatterPost['body']['messageSegments'] = [headerSegment, mentionSegment, messageSegment]
+
+		reqData = json.dumps(chatterPost)
+
+		req = urllib.request.Request(chatterUrl, data=reqData.encode('utf-8'))
+		req.add_header('Content-Type','application/json')
+		req.add_header('Authorization','Bearer '+oauthData['access_token'])
+		try :
+			response = urllib.request.urlopen(req)
+			data = json.loads(response.read().decode('utf-8'))
+			#print ('got chatter data: ', data)
+		except urllib.error.URLError as e:
+			print ('chatter post failed: ',e.reason)
+		
+	else :
+		print ('Failed to authenticate to SFDC.')
+
+
+
+################################################################################
+# PROGRAM ENTRY POINT
+#####################
+
+# get the current IP of the wifi interface.
+currentIp = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
+
+# if the IP has changed, send a notification with the new IP.
+if (hasIpChanged(currentIp)) :
+	sendChangeNotification(currentIp)
+
